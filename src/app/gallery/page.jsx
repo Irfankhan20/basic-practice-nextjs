4647753c0460c18df19f8bf9ed478698fc56cb24
Irfankhan20 <sipathan96@gmail.com>
2025-02-09 00:15:28 +0600

    optimize image using image components

---
+++ src/app/gallery/page.jsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+const GalleryPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-center">Image Optimizations</h1>
+      <h2 className="text-2xl text-center py-1 underline mt-10">
+        Regular Image Tag
+      </h2>
+      <img
+        src="https://www.axelerant.com/hubfs/Imported_Blog_Media/nextjs_image1.jpg"
+        alt="Next image"
+        className="mx-auto"
+        width={500}
+        height={500}
+      />
+      <h2 className="text-2xl text-center py-1 mt-10 underline">
+        Next js image component
+      </h2>
+      <Image
+        src="https://www.axelerant.com/hubfs/Imported_Blog_Media/nextjs_image1.jpg"
+        alt="Next image"
+        width={500}
+        height={500}
+        className="mx-auto"
+      />
+    </div>
+  );
+};
+
+export default GalleryPage;
