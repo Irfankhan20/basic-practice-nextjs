3a0e413e3de4b16d6c7e1ca99b9872b64d144062
Irfankhan20 <sipathan96@gmail.com>
2025-02-10 06:34:11 +0600

    set up json server for data fetching

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,40 @@ export const metadata = {
   title: "1st Project With Next.Js | Home Page",
   description: "This is website home page",
 };
-const HomePage = () => {
+const HomePage = async () => {
+  const res = await fetch("http://localhost:5000/shoes");
+  const shoes = await res.json();
+  // console.log(data);
   return (
     <div>
       <h1>Welcome to Next.Js Home Page</h1>
       <Counter></Counter>
+      <h1 className="text-3xl underline font-bold text-center mt-10 mb-5">
+        All Shoes:
+      </h1>
+      <div className="flex justify-center  gap-20">
+        {shoes.slice(0, 4).map((shoe) => (
+          <div key={shoe.id} className="card bg-base-100 w-96 shadow-xl">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">
+                Shoes!
+                <div className="badge badge-secondary">NEW</div>
+              </h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <div className="badge badge-outline">Fashion</div>
+                <div className="badge badge-outline">Products</div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
